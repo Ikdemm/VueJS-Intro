b6b50e194114ea208f2b9cd6abd3725caddf62dd
Ikdemm <ikdem.benmbarek@aiesec.net>
2020-10-06 18:09:08 +0100

    calculator + select

--- first-app/src/App.vue
+++ first-app/src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row">
+      <input type="text" v-model.number="num1" />
+      <span>+</span>
+      <input type="text" v-model.number="num2" />
+      <span>=</span>
+      <span>{{ num1 + num2 }}</span>
+    </div>
+    <div class="row second">
+      <select v-model="name">
+        <option v-bind:key="framework" v-for="framework in frameworks">
+          {{ framework }}
+        </option>
+      </select>
+      <p>My favorite kind of taco is {{ name }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      name: "React",
+      frameworks: ["Angular", "React", "Vue"],
+    };
+  },
+  // components: {
+  //   HelloWorld
+  // }
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.row {
+  margin: 50px;
+  display: -webkit-box;
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+  font-family: "Work Sans", sans-serif;
+}
+
+input,
+span {
+  padding: 10px;
+  font-size: 30px;
+  font-family: "Work Sans", sans-serif;
+}
+
+input {
+  width: 50px;
+}
+
+span {
+  width: 20px;
+}
+
+.second {
+  font-size: 18px;
+}
+.second p {
+  margin-left: 20px;
 }
 </style>
