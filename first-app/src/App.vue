4ddc3cfe9f2fb540bb3fe99330d40ff564a8ad70
Ikdemm <ikdem.benmbarek@aiesec.net>
2020-10-06 21:39:04 +0100

    added and rendered the second component

--- first-app/src/App.vue
+++ first-app/src/App.vue
@@ -1,31 +1,20 @@
 <template>
   <div id="app">
-    <div class="row">
-      <input type="text" v-model.number="num1" />
-      <span>+</span>
-      <input type="text" v-model.number="num2" />
-      <span>=</span>
-      <span>{{ num1 + num2 }}</span>
-    </div>
+    <Calculator />
     <Framework />
   </div>
 </template>
 
 <script>
 import Framework from "./components/Framework.vue";
+import Calculator from "./components/Calculator.vue";
 
 export default {
   name: "App",
-  data() {
-    return {
-      num1: 0,
-      num2: 0,
-      name: "React",
-      frameworks: ["Angular", "React", "Vue"],
-    };
-  },
+  data() {},
   components: {
     Framework,
+    Calculator,
   },
 };
 </script>
---
+++ first-app/src/components/Calculator.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+    <input type="text" v-model.number="num1" />
+    <span>+</span>
+    <input type="text" v-model.number="num2" />
+    <span>=</span>
+    <span>{{ num1 + num2 }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calculator",
+  props: {},
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+    };
+  },
+};
+</script>
