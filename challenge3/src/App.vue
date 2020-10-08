57391051f1ceb94be0173f1bb0083cb802b0a9e1
Ikdemm <ikdem.benmbarek@aiesec.net>
2020-10-08 11:13:28 +0100

    challenge3 solved

--- challenge3/src/App.vue
+++ challenge3/src/App.vue
@@ -1,30 +1,52 @@
 <template>
   <div id="app">
-    <!-- <h1>Your Upcoming Destinations</h2> -->
-    <div class="location-contain">
-      <div class="locations">
-        <div class="place">
-          <img
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/moscow.svg"
-            width="235"
-            height="300"
-          />
-          <h2>Moscow</h2>
-          <p>
-            Moscow is the capital and most populous city of Russia, with 13.2
-            million residents within the city limits and 17.8 million within the
-            urban area. Moscow has the status of a Russian federal city.
-          </p>
-        </div>
-      </div>
-    </div>
+    <h1>Your Upcoming Destinations</h1>
+    <Destination
+      v-for="location in locations"
+      v-bind:key="location.id"
+      v-bind:location="location"
+    />
   </div>
 </template>
 
 <script>
+import Destination from "@/components/Destination.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Destination,
+  },
+  data() {
+    return {
+      locations: [
+        {
+          id: 0,
+          name: "moscow",
+          img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/moscow.svg",
+          desc: `Moscow is the capital and most populous city of Russia, with 13.2 million residents within the city limits and 17.8 million within the urban area. Moscow has the status of a Russian federal city.`,
+        },
+        {
+          id: 1,
+          name: "paris",
+          img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris1.svg",
+          desc: `Paris is the capital and most populous city of France. By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts, and it retains that position still today.`,
+        },
+        {
+          id: 2,
+          name: "rome",
+          img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/rome.svg",
+          desc: `Rome's history spans more than 2,500 years. While Roman mythology dates the founding of Rome at around 753 BC, the site has been inhabited for much longer, making it one of the oldest continuously occupied sites in Europe.`,
+        },
+        {
+          id: 3,
+          name: "paris",
+          img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris2.svg",
+          desc: `By the end of the 12th century, Paris had become the political, economic, religious, and cultural capital of France. Maurice de Sully undertook the construction of the Notre Dame Cathedral at its eastern extremity.`,
+        },
+      ],
+    };
+  },
 };
 </script>
 
@@ -38,36 +60,5 @@ body {
 
 h1 {
   text-align: center;
-}
-
-.location-contain {
-  display: -webkit-box;
-  display: flex;
-  -webkit-box-pack: center;
-  justify-content: center;
-}
-
-.place {
-  display: -webkit-box;
-  display: flex;
-  -webkit-box-orient: vertical;
-  -webkit-box-direction: normal;
-  flex-direction: column;
-  width: 280px;
-  height: 320px;
-  -webkit-box-pack: center;
-  justify-content: center;
-  background: white;
-  border: 1px solid #ddd;
-  padding: 20px 20px;
-  margin: 10px;
-}
-.place h2 {
-  margin: 0;
-  text-align: center;
-}
-.place img {
-  margin: 10px;
-  align-self: center;
 }
 </style>
---
+++ challenge3/src/components/Destination.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="destination">
+    <div class="location-contain">
+      <div class="locations">
+        <div class="place">
+          <img :src="location.img" width="235" height="300" />
+          <h2>{{ location.name }}</h2>
+          <p>
+            {{ location.desc }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Destination",
+  props: ["location"],
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+body {
+  width: 100vw;
+  height: 100vh;
+  font-family: "NTR", sans-serif;
+  background: #eee;
+}
+
+h1 {
+  text-align: center;
+}
+
+.location-contain {
+  display: -webkit-box;
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+}
+
+.place {
+  display: -webkit-box;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  flex-direction: column;
+  width: 280px;
+  height: 320px;
+  -webkit-box-pack: center;
+  justify-content: center;
+  background: white;
+  border: 1px solid #ddd;
+  padding: 20px 20px;
+  margin: 10px;
+}
+.place h2 {
+  margin: 0;
+  text-align: center;
+}
+.place img {
+  margin: 10px;
+  align-self: center;
+}
+</style>
